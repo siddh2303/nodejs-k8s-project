d805cd77b941089e14e227854c9229e68d2aa48b
siddh2303 <siddhpatel2303@gmail.com>
2024-07-16 10:33:50 +0530

    Add new route

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello, Kubernetes!');
 });
+app.get('/newroute', (req, res) => {
+    res.send('This is a new route!');
+});
 
 app.listen(port, () => {
     console.log(`App running at http://localhost:${port}`);
